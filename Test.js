9961f92b82fcc29dea3c325a63e57e2f9a7cebf6
ClarkGH <clarkhinchcliff@hotmail.com>
2015-02-10 13:06:14 -0700

    created a collection -posts update not being ran when I click on save. Adding in iron router

--- Test.js
+++ Test.js
@@ -14,15 +14,18 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     },
     'click #saveButton': function (evt, tmpl) {
-        MyCollection.update({_id: this._id},{$set:{
+        Posts.update({_id: this._id},{$set:{
           stared: true
         }}, function(error, result){
         if(error){
           HipaaLogger.logEvent("error", Meteor.userId(), Meteor.user().profile.name, "Forms", null, error);
+          console.log("Bad")
         }
         if(result){
           HipaaLogger.logEvent("create", Meteor.userId(), Meteor.user().profile.name, "Forms", self._id, null);
+          console.log("Good");
         }
+        console.log("layer run through")
       });
     }
   });
@@ -30,6 +33,7 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  Posts = new Meteor.Collection("Posts");
   Meteor.startup(function () {
     // code to run on server at startup
   });
