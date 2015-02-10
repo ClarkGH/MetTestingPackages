721b1526378458a3af09f84f1406f7ff78ee2f0d
ClarkGH <clarkhinchcliff@hotmail.com>
2015-02-10 14:26:54 -0700

    removed iron router and added autopublish

--- Test.js
+++ Test.js
@@ -1,7 +1,5 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Meteor.subscribe("posts");
-
   Session.setDefault('counter', 0);
 
   Template.hello.helpers({
@@ -11,38 +9,15 @@ if (Meteor.isClient) {
   });
 
   Template.hello.events({
-    // 'click #incrementButton': function () {
-    //   // increment the counter when button is clicked
-    //   Session.set('counter', Session.get('counter') + 1);
-    // },
-    'click #saveButton': function (evt, tmpl) {
-      console.log("batman")
-      // Posts.update({_id: this._id},{$set:{
-      //   stared: true
-      // }}, function(error, result){
-      // if(error){
-      //   HipaaLogger.logEvent("error", Meteor.userId(), Meteor.user().profile.name, "Forms", null, error);
-      //   console.log("Bad")
-      // }
-      // if(result){
-      //   HipaaLogger.logEvent("create", Meteor.userId(), Meteor.user().profile.name, "Forms", self._id, null);
-      //   console.log("Good");
-      // }
-      // console.log("Got through me")
-    // });
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
     }
   });
-
 }
 
 if (Meteor.isServer) {
-  Posts = new Meteor.Collection("posts");
   Meteor.startup(function () {
     // code to run on server at startup
   });
-
-  Meteor.publish('posts', function () {
-    return Posts.find()
-  });
 }
-
