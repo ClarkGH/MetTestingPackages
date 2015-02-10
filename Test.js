63911453240000539de14dd935ca0de0dee26c5c
ClarkGH <clarkhinchcliff@hotmail.com>
2015-02-10 13:26:47 -0700

    added iron router -getting weird errors, the application looks like it requires autopublish and iron router to be removed

--- Test.js
+++ Test.js
@@ -1,5 +1,7 @@
 if (Meteor.isClient) {
   // counter starts at 0
+  Meteor.subscribe("tasks");
+
   Session.setDefault('counter', 0);
 
   Template.hello.helpers({
@@ -13,29 +15,38 @@ if (Meteor.isClient) {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     },
-    'click #saveButton': function (evt, tmpl) {
-        Posts.update({_id: this._id},{$set:{
-          stared: true
-        }}, function(error, result){
-        if(error){
-          HipaaLogger.logEvent("error", Meteor.userId(), Meteor.user().profile.name, "Forms", null, error);
-          console.log("Bad")
-        }
-        if(result){
-          HipaaLogger.logEvent("create", Meteor.userId(), Meteor.user().profile.name, "Forms", self._id, null);
-          console.log("Good");
-        }
-        console.log("layer run through")
-      });
-    }
+    // 'click #saveButton': function (evt, tmpl) {
+    //   Posts.update({_id: this._id},{$set:{
+    //     stared: true
+    //   }}, function(error, result){
+    //   if(error){
+    //     HipaaLogger.logEvent("error", Meteor.userId(), Meteor.user().profile.name, "Forms", null, error);
+    //     console.log("Bad")
+    //   }
+    //   if(result){
+    //     HipaaLogger.logEvent("create", Meteor.userId(), Meteor.user().profile.name, "Forms", self._id, null);
+    //     console.log("Good");
+    //   }
+    //   console.log("Got through me")
+    // });
+    // }
   });
 
 }
 
 if (Meteor.isServer) {
-  Posts = new Meteor.Collection("Posts");
+  Posts = new Meteor.Collection("posts");
   Meteor.startup(function () {
     // code to run on server at startup
+  });
+  Router.map(function() {
+    this.route('/')
+    path: 'Test'
+    template: 'hello'
+  });
+
+  Meteor.publish("posts", function () {
+    return Posts.find()
   });
 }
 
