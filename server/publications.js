660e64b3409f9bb9039b4784baa08f502683dd5c
ClarkGH <clarkhinchcliff@hotmail.com>
2015-02-10 15:26:14 -0700

    This HIPAA package installs a deprecated version of roles

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,4 @@
 Meteor.publish('hipaa', function () {
-  if (Roles.userIsInRole(this.userId, ['admin'])) {
-    return Hipaa.find();
-  }else{
-    return []; // won't publish a thing if the user doesn't have the right role
+  return Hipaa.find();
   }
-});
+);
